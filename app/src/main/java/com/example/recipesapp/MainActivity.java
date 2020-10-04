f88ccb1f7ed48fc1cef2c6ab7cf22d61aa2be66a
igormagun <32044854+igormagun@users.noreply.github.com>
2020-10-04 13:37:32 -0400

    Added code to initialize the RecyclerView in the MainActivity, fixed layout width and height

--- app/src/main/java/com/example/recipesapp/MainActivity.java
+++ app/src/main/java/com/example/recipesapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.recipesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the ArrayLists
+        ArrayList<String> recipeNames = new ArrayList<>(Arrays.asList(
+                "Test", "One", "Two", "Three"));
+        ArrayList<String> recipeShortDescriptions = new ArrayList<>();
+        ArrayList<String> recipes = new ArrayList<>();
+        ArrayList<String> images = new ArrayList<>();
+
+        // Fill the lists of short descriptions, recipes, and images
+        for (int i = 0; i < recipeNames.size(); i++) {
+            recipeShortDescriptions.add("This is a short description for this delicious recipe");
+            // TODO: Replace with longer recipe text
+            recipes.add("");
+            // TODO: Replace with real image IDs
+            images.add("");
+        }
+
+        // Create the RecylerView and corresponding adapter
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecipeListAdapter listAdapter = new RecipeListAdapter(recipeNames, recipeShortDescriptions,
+                recipes, images, this);
+
+        // Set the adapter and layout manager for the RecyclerView
+        recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
